5a3c9a85daee50c63607aff2b74e27fd93eae132
Erick <yiperick@hotmail.com>
2020-01-16 23:57:23 -0700

    commit

---
+++ src/WinnerMessage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar(props) {
+    return (
+        <header className="container-fluid fixed-top" >
+            <div className="row">
+                <h1 className="col-sm-8">You Win!</h1>
+            </div>
+        </header>        
+    )
+}
+
+export default Navbar;
